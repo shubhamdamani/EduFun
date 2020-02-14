aa929480983b26d4882b30c2f8aa54e53ce648c4
IamUtkarshGupta <utkarsh3210@gmail.com>
2020-02-14 21:22:21 +0530

    Classification updated

--- app/src/main/java/com/example/droidrun/FingerPath.java
+++ app/src/main/java/com/example/droidrun/FingerPath.java
@@ -1,4 +1,16 @@
 package com.example.droidrun;
 
+import android.graphics.Path;
+
 public class FingerPath {
+
+    public int color;
+    public int inkWidth;
+    public Path path;
+
+    public FingerPath(int color, int strokeWidth, Path path) {
+        this.color = color;
+        this.inkWidth = strokeWidth;
+        this.path = path;
+    }
 }
