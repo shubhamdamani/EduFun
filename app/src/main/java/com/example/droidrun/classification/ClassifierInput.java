3aa702e737827474fcf9f197d6eed4e511ad5faf
IamUtkarshGupta <utkarsh3210@gmail.com>
2020-02-14 20:48:41 +0530

    Assets and raw added1

--- app/src/main/java/com/example/droidrun/classification/ClassifierInput.java
+++ app/src/main/java/com/example/droidrun/classification/ClassifierInput.java
@@ -1,4 +1,65 @@
 package com.example.droidrun.classification;
 
-public class ClassifierInput {
+
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.ArrayList;
+import java.util.Arrays;
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
+import android.util.Log;
+import com.example.droidrun.MainActivity;
+
+
+/**
+ * Helper class to read TensorFlow model and labels from file
+ */
+abstract class ClassifierInput {
+
+    /*
+     * Reads the compressed model as MappedByteBuffer from file.
+     *
+     */
+    public static MappedByteBuffer loadModel(Activity activity, String model) throws IOException {
+        AssetManager assetManager = activity.getAssets();
+        AssetFileDescriptor fileDescriptor = assetManager.openFd(model);
+        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
+        FileChannel fileChannel = inputStream.getChannel();
+        long startOffset = fileDescriptor.getStartOffset();
+        long declaredLength = fileDescriptor.getDeclaredLength();
+        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
+    }
+
+    /*
+     * Read labels from file to an array list of labels.
+     * This list represents the mapping from index position to label.
+     *
+     */
+    public static ArrayList readImageLabels(Activity activity, String File) {
+        AssetManager assetManager = activity.getAssets();
+        ArrayList<String> result = new ArrayList<>();
+        try {
+            InputStream is = assetManager.open(File);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is));
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] arr  = line.split(",");
+                result.add(arr[1]);
+            }
+
+            return result;
+        } catch (IOException ex) {
+            throw new IllegalStateException("Cannot read labels from " + File + " " + ex);
+        }
+    }
 }
+
+
--- app/src/main/java/com/example/droidrun/classification/ClassifierResult.java
+++ app/src/main/java/com/example/droidrun/classification/ClassifierResult.java
@@ -1,4 +1,80 @@
 package com.example.droidrun.classification;
 
+import android.util.ArrayMap;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Readable representation of the models output vector.
+ * Contains labelName, labelName position in result vector and prob.
+ */
 public class ClassifierResult {
+    private String labelName;
+    private int labelIndex;
+    private float prob;
+    private List<Integer> topKList; // contains the index
+
+    public ClassifierResult(float[] result, ArrayList<String> labels) {
+        this.labelIndex = findMaxProbIndex(result); // set index position
+        this.prob = result[labelIndex]; // set prob
+        this.labelName = labels.get(labelIndex); // search for labelName
+        this.topKList = getTopkLabels(3, result);
+
+    }
+
+    // find the index with the maximum prob
+    private int findMaxProbIndex(float[] result) {
+        float maxProb = 0.0f;
+        int maxIndex = -1;
+        for (int i = 0; i < result.length; i++) {
+            if (result[i] > maxProb) {
+                maxProb = result[i];
+                maxIndex = i;
+            }
+        }
+        if (maxIndex == -1) {
+            Log.e("Result class", "findMaxProbIndex found no maximum");
+        }
+        return maxIndex;
+    }
+
+
+    // returns the top k labels with prob
+    private List<Integer> getTopkLabels(int k, float[] result) {
+        List<Integer> topKList = new LinkedList<>();
+        for (int j = 0; j < k; j++) {
+            float maxProb = 0.0f;
+            int maxIndex = -1;
+            for (int i = 0; i < result.length; i++) {
+                if (!topKList.contains(i)) {
+                    if (result[i] > maxProb) {
+                        maxProb = result[i];
+                        maxIndex = i;
+                    }
+                }
+            }
+            topKList.add(maxIndex);
+        }
+
+        return topKList;
+    }
+
+    public String getLabel() { return this.labelName; }
+
+    public float getProbability() {
+        return this.prob;
+    }
+
+    public List<Integer> getTopK() {
+        return this.topKList;
+    }
+
+    public int getLabelIndex() {
+        return this.labelIndex;
+    }
 }
+
