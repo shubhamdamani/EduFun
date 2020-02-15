c04e4cd6f2cacac101b6101e0cd8480883bc0097
IamUtkarshGupta <utkarsh3210@gmail.com>
2020-02-15 20:33:00 +0530

    Quiz

--- app/src/main/java/com/example/droidrun/MainMenu.java
+++ app/src/main/java/com/example/droidrun/MainMenu.java
@@ -1,6 +1,10 @@
 package com.example.droidrun;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,6 +13,10 @@ import android.widget.Button;
 public class MainMenu extends AppCompatActivity {
 
     Button multiplayer,singleplayer,inst,exi,more,affair;
+    MediaPlayer media;
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +29,7 @@ public class MainMenu extends AppCompatActivity {
         exi=findViewById(R.id.exit);
         more = findViewById(R.id.more);
         affair=findViewById(R.id.affair);
-
+        media = MediaPlayer.create(this, R.raw.tada);
         affair.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,5 +80,40 @@ public class MainMenu extends AppCompatActivity {
             }
         });
 
+        // ShakeDetector initialization
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @Override
+            public void onShake(int count) {
+                /*
+                 * The following method, "handleShakeEvent(count):" is a stub //
+                 * method you would use to setup whatever you want done once the
+                 * device has been shook.
+                 */
+                media.start();
+                Intent intent = new Intent(MainMenu.this,ObjectActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
+    }
+
 }
